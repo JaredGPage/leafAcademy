a3a49ecc56c2ab9d9ffe9226e062d2f1ef0816b9
Jared Page <jared.page10@gmail.com>
2024-07-12 10:13:36 +0200

    add sentry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,11 +1,42 @@
+import * as Sentry from "@sentry/node";
 import { PassThrough } from "node:stream";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import type { EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
+import * as isbotModule from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
-import type { EntryContext } from "@remix-run/node";
 
-const ABORT_DELAY = 5000; // Adjusted for shorter timeout
+export function handleError(error: unknown, { request }: { request: Request }) {
+  Sentry.captureException(error, {
+    contexts: {
+      request: {
+        url: request.url,
+        method: request.method,
+        headers: request.headers,
+      },
+    },
+  });
+}
+
+Sentry.init({
+  dsn: "https://65147582886161ae65c06a5d8a134a0b@o4507587798237184.ingest.de.sentry.io/4507587802234960",
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+interface Context {
+  res: {
+    status: number;
+    body: string;
+  };
+}
+
+const ABORT_DELAY = 5_000;
 
 export default function handleRequest(
   request: Request,
@@ -13,11 +44,40 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const handler = isbot(request.headers.get("user-agent") || "")
-    ? handleBotRequest
-    : handleBrowserRequest;
+  return isBotRequest(request.headers.get("user-agent"))
+    ? handleBotRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      )
+    : handleBrowserRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      );
+}
 
-  return handler(request, responseStatusCode, responseHeaders, remixContext);
+// We have some Remix apps in the wild already running with isbot@3 so we need
+// to maintain backwards compatibility even though we want new apps to use
+// isbot@4.  That way, we can ship this as a minor Semver update to @remix-run/dev.
+function isBotRequest(userAgent: string | null) {
+  if (!userAgent) {
+    return false;
+  }
+
+  // isbot >= 3.8.0, >4
+  if ("isbot" in isbotModule && typeof isbotModule.isbot === "function") {
+    return isbotModule.isbot(userAgent);
+  }
+
+  // isbot < 3.8.0
+  if ("default" in isbotModule && typeof isbotModule.default === "function") {
+    return isbotModule.default(userAgent);
+  }
+
+  return false;
 }
 
 function handleBotRequest(
@@ -26,41 +86,8 @@ function handleBotRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  return handleRequestInternal(
-    request,
-    responseStatusCode,
-    responseHeaders,
-    remixContext,
-    true
-  );
-}
-
-function handleBrowserRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-) {
-  return handleRequestInternal(
-    request,
-    responseStatusCode,
-    responseHeaders,
-    remixContext,
-    false
-  );
-}
-
-function handleRequestInternal(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _isBot: boolean
-) {
   return new Promise((resolve, reject) => {
     let shellRendered = false;
-
     const { pipe, abort } = renderToPipeableStream(
       <RemixServer
         context={remixContext}
@@ -84,6 +111,9 @@ function handleRequestInternal(
 
           pipe(body);
         },
+        onShellError(error: unknown) {
+          reject(error);
+        },
         onError(error: unknown) {
           responseStatusCode = 500;
           // Log streaming rendering errors from inside the shell.  Don't log
@@ -92,7 +122,56 @@ function handleRequestInternal(
           if (shellRendered) {
             console.error(error);
           }
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        onShellReady() {
+          shellRendered = true;
+          const body = new PassThrough();
+          const stream = createReadableStreamFromReadable(body);
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
           reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error);
+          }
         },
       }
     );
